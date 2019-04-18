7f6984af7aae3ac35fb0a7baef08afedb6b31534
OverLander422569 <49753693+OverLander422569@users.noreply.github.com>
2019-04-18 16:01:35 +0200

    Add files via upload
    Version sans les ajout qui font bloquer la liste alphabetique

---
+++ ExamenJavascriptG13 - version 2/JS/script.js
@@ -0,0 +1,36 @@
+
+
+//récupérer et afficher les infos d'un media
+
+$('#listAlphabet').listnav();
+
+/*$('#').click(function() {
+    show(#info)
+});*/
+$('#demoSix').listnav({
+    filterSelector: '.last-name',
+    includeNums: false,
+    removeDisabled: true,
+    allText: 'Complete Cast of Star Trek TNG'
+});
+
+//ajouter un media
+
+/*$("#listAlphabet").on("click", function () {
+    $("#myList").append($("<li>").attr('id', 'ElN1').attr('class', 'myEl').text("rrrr"));
+});
+$("#myList").on("click", ".myEl", function(event){
+    alert($("#ElN1").attr('id'));
+});*/
+
+//
+
+//modifier les infos
+
+//supprimer le média de la liste
+
+//afficher alphabétiquement
+
+//afficher par le type
+
+//afficher (si prêté est coché) avec les infos comme le nom de la personne
